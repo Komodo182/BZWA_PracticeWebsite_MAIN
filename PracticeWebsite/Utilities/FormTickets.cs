96fc15a6282f37065380db3f9b229eb47921ab0e
s2301171 <s2301171@NDC03213>
2024-10-18 13:05:56 +0100

    Fixed the big problem wih the register page but now issue with show password however now need to focus on the rest of the page such as main,etc.

---
+++ PracticeWebsite/Utilities/FormTickets.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracticeWebsite.Utilities
+{
+    public class RegisterAccountForm
+    {
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Length of the name can't be more than 15 characters long.")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(20, ErrorMessage = "Length of the name can't be more than 20 characters long.")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(8, ErrorMessage = "Username length can't be more than 10 charcaters long.")]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Password must be at least 8 characters long.", MinimumLength = 8)]
+        public string Password { get; set; }
+        [Required]
+        [Compare(nameof(Password))]
+        public string Password2 { get; set; }
+        [Required]
+        [StringLength(8, ErrorMessage = "The postcode is invalid length.", MinimumLength = 8)]
+        public string PostCode { get; set; }
+        [Required]
+        [StringLength(11, ErrorMessage = "Invalid phone number length.", MinimumLength = 11)]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [StringLength(8, ErrorMessage = "Invalid date has been provided.")]
+        public string DoB { get; set; }
+        public bool employeeStatus { get; set; }
+        [StringLength(12, ErrorMessage = "Please input your Employee identification code.", MinimumLength = 12)]
+        public string EmployeeID { get; set; }
+    }
+
+    //private void OnValidSubmit(EditContext context)
+    //{
+    //    success = true;
+    //    StateHasChanged();
+    //    Console.WriteLine("Register button has been clicked");
+    //    Console.WriteLine(model.Username);
+    //    Console.WriteLine(model.Password);
+    //    Console.WriteLine(model.FirstName);
+    //    Console.WriteLine(model.LastName);
+    //    Console.WriteLine(model.Email);
+    //    Console.WriteLine(model.PostCode);
+    //    Console.WriteLine(model.PhoneNumber);
+    //    Console.WriteLine(model.DoB);
+    //    Console.WriteLine(model.employeeStatus);
+    //    Console.WriteLine(model.EmployeeID);
+    //}
+}
