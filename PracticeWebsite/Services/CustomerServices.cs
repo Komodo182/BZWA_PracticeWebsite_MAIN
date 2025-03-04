776c431b9a9de58a5f916b138a90f777739eddd5
s2301171 <s2301171@NDC03213>
2025-03-04 10:51:06 +0000

    I fixed it mostly the algorithms are functioning correctly however there is an issue with the projection of the data onto the mud stpper so the algorithm of line 649 on Maps.razor seems to run however i dont quite use it or dosent send the values to SQLquizdata which is at line 1174 of the same file.

--- PracticeWebsite/Services/CustomerServices.cs
+++ PracticeWebsite/Services/CustomerServices.cs
@@ -56,7 +56,7 @@ namespace PracticeWebsite.Services
         {
             var questions = await _context.Questions
                 .Where(q => q.QuizId == QuizId)
-                .Take(3) // Assuming you want only 3 questions max
+                .Take(10) // Assuming you want only 3 questions max
                 .ToListAsync();
 
             if (questions.Count < 3) return new quiz(); // Ensure we don't get errors
@@ -68,39 +68,72 @@ namespace PracticeWebsite.Services
                 Quiz1Ans2 = questions[0].Answer2,
                 Quiz1Ans3 = questions[0].Answer3,
                 Quiz1Ans4 = questions[0].Answer4,
+                Quiz1TrueAns = questions[0].Finalanswer,
 
                 Quiz2Question = questions[1].Question1,
                 Quiz2Ans1 = questions[1].Answer1,
                 Quiz2Ans2 = questions[1].Answer2,
                 Quiz2Ans3 = questions[1].Answer3,
                 Quiz2Ans4 = questions[1].Answer4,
+                Quiz2TrueAns = questions[1].Finalanswer,
 
                 Quiz3Question = questions[2].Question1,
                 Quiz3Ans1 = questions[2].Answer1,
                 Quiz3Ans2 = questions[2].Answer2,
                 Quiz3Ans3 = questions[2].Answer3,
                 Quiz3Ans4 = questions[2].Answer4,
+                Quiz3TrueAns = questions[2].Finalanswer,
+
+                Quiz4Question = questions[3].Question1,
+                Quiz4Ans1 = questions[3].Answer1,
+                Quiz4Ans2 = questions[3].Answer2,
+                Quiz4Ans3 = questions[3].Answer3,
+                Quiz4Ans4 = questions[3].Answer4,
+                Quiz4TrueAns = questions[3].Finalanswer,
+
+                Quiz5Question = questions[4].Question1,
+                Quiz5Ans1 = questions[4].Answer1,
+                Quiz5Ans2 = questions[4].Answer2,
+                Quiz5Ans3 = questions[4].Answer3,
+                Quiz5Ans4 = questions[4].Answer4,
+                Quiz5TrueAns = questions[4].Finalanswer,
+
+                Quiz6Question = questions[5].Question1,
+                Quiz6Ans1 = questions[5].Answer1,
+                Quiz6Ans2 = questions[5].Answer2,
+                Quiz6Ans3 = questions[5].Answer3,
+                Quiz6Ans4 = questions[5].Answer4,
+                Quiz6TrueAns = questions[5].Finalanswer,
+
+                Quiz7Question = questions[6].Question1,
+                Quiz7Ans1 = questions[6].Answer1,
+                Quiz7Ans2 = questions[6].Answer2,
+                Quiz7Ans3 = questions[6].Answer3,
+                Quiz7Ans4 = questions[6].Answer4,
+                Quiz7TrueAns = questions[6].Finalanswer,
+
+                Quiz8Question = questions[7].Question1,
+                Quiz8Ans1 = questions[7].Answer1,
+                Quiz8Ans2 = questions[7].Answer2,
+                Quiz8Ans3 = questions[7].Answer3,
+                Quiz8Ans4 = questions[7].Answer4,
+                Quiz8TrueAns = questions[7].Finalanswer,
+
+                Quiz9Question = questions[8].Question1,
+                Quiz9Ans1 = questions[8].Answer1,
+                Quiz9Ans2 = questions[8].Answer2,
+                Quiz9Ans3 = questions[8].Answer3,
+                Quiz9Ans4 = questions[8].Answer4,
+                Quiz9TrueAns = questions[8].Finalanswer,
+
+                Quiz10Question = questions[9].Question1,
+                Quiz10Ans1 = questions[9].Answer1,
+                Quiz10Ans2 = questions[9].Answer2,
+                Quiz10Ans3 = questions[9].Answer3,
+                Quiz10Ans4 = questions[9].Answer4,
+                Quiz10TrueAns = questions[9].Finalanswer,
+
             };
-        }
-        public class quiz
-        {
-            public string Quiz1Question { get; set; }
-            public string Quiz1Ans1 { get; set; }
-            public string Quiz1Ans2 { get; set; }
-            public string Quiz1Ans3 { get; set; }
-            public string Quiz1Ans4 { get; set; }
-
-            public string Quiz2Question { get; set; }
-            public string Quiz2Ans1 { get; set; }
-            public string Quiz2Ans2 { get; set; }
-            public string Quiz2Ans3 { get; set; }
-            public string Quiz2Ans4 { get; set; }
-
-            public string Quiz3Question { get; set; }
-            public string Quiz3Ans1 { get; set; }
-            public string Quiz3Ans2 { get; set; }
-            public string Quiz3Ans3 { get; set; }
-            public string Quiz3Ans4 { get; set; }
         }
     }
 }
---
+++ PracticeWebsite/Utilities/Classes.cs
@@ -0,0 +1,77 @@
+﻿namespace PracticeWebsite.Utilities
+{
+    public class quiz
+    {
+        public string Quiz1Question { get; set; }
+        public string Quiz1Ans1 { get; set; }
+        public string Quiz1Ans2 { get; set; }
+        public string Quiz1Ans3 { get; set; }
+        public string Quiz1Ans4 { get; set; }
+
+        public string Quiz2Question { get; set; }
+        public string Quiz2Ans1 { get; set; }
+        public string Quiz2Ans2 { get; set; }
+        public string Quiz2Ans3 { get; set; }
+        public string Quiz2Ans4 { get; set; }
+
+        public string Quiz3Question { get; set; }
+        public string Quiz3Ans1 { get; set; }
+        public string Quiz3Ans2 { get; set; }
+        public string Quiz3Ans3 { get; set; }
+        public string Quiz3Ans4 { get; set; }
+
+        public string Quiz4Question { get; set; }
+        public string Quiz4Ans1 { get; set; }
+        public string Quiz4Ans2 { get; set; }
+        public string Quiz4Ans3 { get; set; }
+        public string Quiz4Ans4 { get; set; }
+
+        public string Quiz5Question { get; set; }
+        public string Quiz5Ans1 { get; set; }
+        public string Quiz5Ans2 { get; set; }
+        public string Quiz5Ans3 { get; set; }
+        public string Quiz5Ans4 { get; set; }
+
+        public string Quiz6Question { get; set; }
+        public string Quiz6Ans1 { get; set; }
+        public string Quiz6Ans2 { get; set; }
+        public string Quiz6Ans3 { get; set; }
+        public string Quiz6Ans4 { get; set; }
+
+        public string Quiz7Question { get; set; }
+        public string Quiz7Ans1 { get; set; }
+        public string Quiz7Ans2 { get; set; }
+        public string Quiz7Ans3 { get; set; }
+        public string Quiz7Ans4 { get; set; }
+
+        public string Quiz8Question { get; set; }
+        public string Quiz8Ans1 { get; set; }
+        public string Quiz8Ans2 { get; set; }
+        public string Quiz8Ans3 { get; set; }
+        public string Quiz8Ans4 { get; set; }
+
+        public string Quiz9Question { get; set; }
+        public string Quiz9Ans1 { get; set; }
+        public string Quiz9Ans2 { get; set; }
+        public string Quiz9Ans3 { get; set; }
+        public string Quiz9Ans4 { get; set; }
+
+        public string Quiz10Question { get; set; }
+        public string Quiz10Ans1 { get; set; }
+        public string Quiz10Ans2 { get; set; }
+        public string Quiz10Ans3 { get; set; }
+        public string Quiz10Ans4 { get; set; }
+
+        public string Quiz1TrueAns { get; set; }
+        public string Quiz2TrueAns { get; set; }
+        public string Quiz3TrueAns { get; set; }
+        public string Quiz4TrueAns { get; set; }
+        public string Quiz5TrueAns { get; set; }
+        public string Quiz6TrueAns { get; set; }
+        public string Quiz7TrueAns { get; set; }
+        public string Quiz8TrueAns { get; set; }
+        public string Quiz9TrueAns { get; set; }
+        public string Quiz10TrueAns { get; set; }
+    }
+}
+
